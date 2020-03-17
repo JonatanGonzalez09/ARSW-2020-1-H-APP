1b3cfd9673dfa2ef467d87b17472f2929d637e4d
Juan Alberto Mejia S <jualme9@hotmail.com>
2020-03-16 20:30:26 -0500

    Implementation of HappNurseAssistantStub

--- Happ/src/main/java/edu/eci/arsw/happ/persistence/impl/InMemoryHappNurseAssistantPersistence.java
+++ Happ/src/main/java/edu/eci/arsw/happ/persistence/impl/InMemoryHappNurseAssistantPersistence.java
@@ -5,12 +5,58 @@
  */
 package edu.eci.arsw.happ.persistence.impl;
 
+import edu.eci.arsw.happ.model.Floor;
+import edu.eci.arsw.happ.model.Nurse;
+import edu.eci.arsw.happ.model.Patient;
+import edu.eci.arsw.happ.model.Task;
 import edu.eci.arsw.happ.persistence.HappNurseAssistantPersistence;
+import edu.eci.arsw.happ.persistence.HappPersistenceException;
+import java.util.List;
 
 /**
  *
  * @author jualme
  */
 public class InMemoryHappNurseAssistantPersistence implements HappNurseAssistantPersistence {
+
+    @Override
+    public Nurse getCurrentNurseAssistant(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Patient> getPatients(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Patient getPatient(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Floor getFloor(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Nurse getBoss(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Task> getTasks(String idDocument) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Task getTask(String idDocument, String taskId) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void updateTask(String idDocument, String taskId, Task newts) throws HappPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
--- Happ/src/main/java/edu/eci/arsw/happ/service/impl/HappNurseAssistantStub.java
+++ Happ/src/main/java/edu/eci/arsw/happ/service/impl/HappNurseAssistantStub.java
@@ -17,51 +17,96 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  *
- * @author jualm
+ * @author jualme
  */
 public class HappNurseAssistantStub implements HappNurseAssistant {
     
     @Autowired
-    HappNurseAssistantPersistence Hp = null;
-
+    HappNurseAssistantPersistence Hp;
+    
+    
+    /**
+     * Metodo encargado de regresasr la informacion de la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @throws HappPersistenceException 
+     */
     @Override
     public Nurse getCurrentNurseAssistant(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getCurrentNurseAssistant(idDocument);
     }
 
+    
+    /**
+     * Metodo encargado de regresasr la informacion de los pacientes asignados a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @throws HappPersistenceException 
+     */    
     @Override
     public List<Patient> getPatients(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getPatients(idDocument);
     }
 
+    /**
+     * Metodo encargado de regresasr la informacion del paciente asignado a la enfermera asistente
+     * @param idDocument documento de identidad del paciente
+     * @throws HappPersistenceException 
+     */ 
     @Override
     public Patient getPatient(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getPatient(idDocument);
     }
-
+    
+     /**
+     * Metodo encargado de regresasr la informacion del piso asignado a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @throws HappPersistenceException 
+     */ 
     @Override
     public Floor getFloor(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getFloor(idDocument);        
     }
 
+    /**
+     * Metodo encargado de regresasr la informacion del jefe asignado a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @throws HappPersistenceException 
+     */     
     @Override
     public Nurse getBoss(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getBoss(idDocument);
     }
 
+    /**
+     * Metodo encargado de regresasr la informacion de las tareas asignadas a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @throws HappPersistenceException 
+     */    
     @Override
     public List<Task> getTasks(String idDocument) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getTasks(idDocument);
     }
 
+    /**
+     * Metodo encargado de regresasr la informacion de la tarea asignada a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @param taskId Id de la tarea a consultar
+     * @throws HappPersistenceException 
+     */
     @Override
     public Task getTask(String idDocument, String taskId) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return Hp.getTask(idDocument, taskId);
     }
 
+    /**
+     * Metodo encargado de actualizar la informacion de la tarea asignada a la enfermera asistente
+     * @param idDocument documento de identidad de la enfermera
+     * @param taskId Id de la tarea a actualizar
+     * @param newts la informacion de la nueva tarea
+     * @throws HappPersistenceException 
+     */
     @Override
     public void updateTask(String idDocument, String taskId, Task newts) throws HappPersistenceException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Hp.updateTask(idDocument, taskId, newts);
     }
     
 }

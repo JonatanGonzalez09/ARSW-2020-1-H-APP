ecb4d24a6a1632cb539593e9037624d0e05b0869
Jmjimmy20 <you@example.com>
2020-03-24 17:32:56 -0500

    token

---
+++ Happ/src/main/java/edu/eci/arsw/security/CORSFilter.java
@@ -0,0 +1,64 @@
+package edu.eci.arsw.security;
+
+import java.util.Arrays;
+import java.util.List;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import com.fasterxml.classmate.Filter;
+
+
+public class CORSFilter implements Filter {
+
+    // This is to be replaced with a list of domains allowed to access the server
+  //You can include more than one origin here
+    private final List<String> allowedOrigins = Arrays.asList("http://localhost:3000"); 
+
+    public void destroy() {
+
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        // Lets make sure that we are working with HTTP (that is, against HttpServletRequest and HttpServletResponse objects)
+        if (req instanceof HttpServletRequest && res instanceof HttpServletResponse) {
+            HttpServlet request = (HttpServlet) req;
+            HttpServletResponse response = (HttpServletResponse) res;
+
+            // Access-Control-Allow-Origin
+            String origin = ((HttpServletRequest) request).getHeader("Origin");
+            response.setHeader("Access-Control-Allow-Origin", allowedOrigins.contains(origin) ? origin : "");
+            response.setHeader("Vary", "Origin");
+
+            // Access-Control-Max-Age
+            response.setHeader("Access-Control-Max-Age", "3600");
+
+            // Access-Control-Allow-Credentials
+            response.setHeader("Access-Control-Allow-Credentials", "true");
+
+            // Access-Control-Allow-Methods
+            response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+
+            // Access-Control-Allow-Headers
+            response.setHeader("Access-Control-Allow-Headers",
+                "Origin, X-Requested-With, Content-Type, Accept, " + "X-CSRF-TOKEN");
+        }
+
+        chain.doFilter(req, res);
+    }
+
+    public void init(FilterChain filterConfig) {
+    }
+
+    @Override
+    public boolean include(Object arg0) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+}

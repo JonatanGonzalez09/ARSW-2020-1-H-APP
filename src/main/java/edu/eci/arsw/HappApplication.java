1690260f7c190ad7ab94075963f56ffb8505ec63
Juan Alberto Mejia S <jualme9@hotmail.com>
2020-05-07 12:18:29 -0500

    added cache to manager

--- src/main/java/edu/eci/arsw/HappApplication.java
+++ src/main/java/edu/eci/arsw/HappApplication.java
@@ -2,8 +2,10 @@ package edu.eci.arsw;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class HappApplication {
 
 	public static void main(String[] args) {
--- src/main/java/edu/eci/arsw/service/NurseManagerService.java
+++ src/main/java/edu/eci/arsw/service/NurseManagerService.java
@@ -6,6 +6,8 @@ import java.util.List;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import edu.eci.arsw.model.*;
@@ -78,11 +80,13 @@ public class NurseManagerService {
 		return nursePersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "nurse", key= "#id")
 	public Nurse getNurse(int id) {
 		return nursePersistence.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
 	}
 	
+	@CachePut(cacheNames= "nurse", key= "#nurse.nurseId")
 	public Nurse updateNurse(Nurse nurse) {
 		Nurse tmpNurse = nursePersistence.findById(nurse.getNurseId())
 			.orElseThrow(() -> new EntityNotFoundException(String.valueOf(nurse.getNurseId())));
@@ -91,6 +95,7 @@ public class NurseManagerService {
 		return nursePersistence.save(tmpNurse);		
 	}
 
+	@CachePut(cacheNames= "nurse_block", key= "#blockId")
 	public List<Nurse> getNursesByBlockId(int blockId) {
 		return nursePersistence.getNursesByBlockId(blockId);
 	}
@@ -99,23 +104,18 @@ public class NurseManagerService {
 		return nursePersistence.getNursByBlockIdAndDate(blockId, date);
 	}
 
+	@CachePut(cacheNames= "nurse", key= "#blockId")
 	public List<Nurse> getNursesByBlockIdToday(int blockId) {
 		return nursePersistence.getNursByBlockIdToday(blockId);
 	}
 
-	public List<Undergoes> getNurseUndergoesByBlockIdAndDate(int blockId, Date date) {
-		return undergoesPersistence.getUndergoesByBlockIdAndDate(blockId, date);
-	}
-
-	public List<Undergoes> getUndergoesTodayByNurseId(int nurseId) {
-		return undergoesPersistence.getTodayUndergoesByNurseId(nurseId);
-	}
 
 	//-----------------Oncall----------------------
 	public List<Oncall> getAllOncalls(){
 		return oncallPersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "oncall", key= "#id")
 	public Oncall getOncall(int id) {
 		return oncallPersistence.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
@@ -129,18 +129,22 @@ public class NurseManagerService {
 		return oncallPersistence.findAllByOncallstartBefore(date);
 	}
 	
+	@Cacheable(cacheNames= "oncall_Block", key= "#block.blockId")
 	public List<Oncall> getOnCallsByBlockId(Block block) {
 		return oncallPersistence.findByBlock(block);
 	}
 
+	@Cacheable(cacheNames= "oncall_Nurse", key= "#nurse.nurseId")
 	public List<Oncall> getOnCallsByNurseId(Nurse nurse) {
 		return oncallPersistence.findByNurse(nurse);
 	}
 
+	@Cacheable(cacheNames= "oncall", key= "#oncall.oncallId")
 	public Oncall setOncall(Oncall oncall) {
 		return oncallPersistence.save(oncall);
 	}
-
+	
+	@CachePut(cacheNames= "oncall", key= "#oncall.oncallId")
 	public Oncall updateOncall(Oncall oncall) {
 		Oncall tmpOncall = oncallPersistence.findById(oncall.getOncallId())
 			.orElseThrow(() -> new EntityNotFoundException(String.valueOf(oncall.getOncallId())));
@@ -154,6 +158,7 @@ public class NurseManagerService {
 		return patienPersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "patient", key= "#id")
 	public Patient getPatient(int id) {
 		return patienPersistence.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
@@ -163,6 +168,7 @@ public class NurseManagerService {
 		return patienPersistence.findByGovTypeAndGovId(type, patientId);
 	}
 
+	@Cacheable(cacheNames= "patient_Nurse", key= "#nurse.nurseId")
 	public Object getPatientsByNurseId(Nurse nurse) {
 		return patienPersistence.getAllPatientsFromNurse(nurse.getNurseId());
 	}
@@ -172,6 +178,7 @@ public class NurseManagerService {
 		return procedurePersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "procedure", key= "#idProcedure")
 	public Procedure getProcedure(int idProcedure) {
 		return procedurePersistence.findById(idProcedure)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(idProcedure)));
@@ -228,6 +235,7 @@ public class NurseManagerService {
 		return stayPersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "stay", key= "#id")
 	public Stay getStay(int id) {
 		return stayPersistence.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));	
@@ -241,6 +249,7 @@ public class NurseManagerService {
 		return stayPersistence.getStaysNotEnd();
 	}
 
+	@Cacheable(cacheNames= "stay_patient", key= "#patient.patientId")
 	public List<Stay> getStaysBypatientId(Patient patient) {
 		return stayPersistence.findByPatient(patient);
 	}
@@ -249,10 +258,12 @@ public class NurseManagerService {
 		return stayPersistence.findByBed(bed);
 	}
 	
+	@Cacheable(cacheNames= "stay", key= "#stay.stayId")
 	public Stay setStay(Stay stay) {
 		return stayPersistence.save(stay);
 	}
 	
+	@CachePut(cacheNames= "stay", key= "#stay.stayId")
 	public Stay updateStay(Stay stay) {
 		Stay tmpStay = stayPersistence.findById(stay.getStayId())
 		.orElseThrow(() -> new EntityNotFoundException(String.valueOf(stay.getStayId())));
@@ -268,47 +279,74 @@ public class NurseManagerService {
 		return undergoesPersistence.findAll();
 	}
 	
+	@Cacheable(cacheNames= "undergoes", key= "#id")
 	public Undergoes getUndergoes(int id) {
+		System.out.println("Enter to undergoes");
 		return undergoesPersistence.findById(id)
 				.orElseThrow(() -> new EntityNotFoundException(String.valueOf(id)));
 	}
 
+	@Cacheable(cacheNames= "undergoes_date", key= "{Today: #date}")
 	public List<Undergoes> getUndergoesToday(Nurse nurse) {
+		System.out.println("Enter to today");
 		return undergoesPersistence.getUndergoesToday(nurse.getNurseId());
 	}
-
+	
+	
+	@Cacheable(cacheNames= "undergoes_date", key= "{After: #date}")
 	public List<Undergoes> getUndergoesAfterToday(Date date) {
+		System.out.println("Enter to after today");
 		return undergoesPersistence.findAllByDateAfter(date);
 	}
 
+	@Cacheable(cacheNames= "undergoes_date", key= "{Before: #date}")
 	public List<Undergoes> getUndergoesBeforeToday(Date date) {
+		System.out.println("Enter to before today");
 		return undergoesPersistence.findAllByDateBefore(date);
 	}
 
+	@Cacheable(cacheNames= "undergoes_nurse", key= "#nurse.nurseId")
 	public List<Undergoes> getUndergoesByNurseId(Nurse nurse) {
+		System.out.println("Enter to nurse");
 		return undergoesPersistence.findByNurse(nurse);
 	}
 
+	@Cacheable(cacheNames= "undergoes_procedure", key= "#procedure.procedureId")
 	public List<Undergoes> getUndergoesByProcedureId(Procedure procedure) {
 		return undergoesPersistence.findByProcedure(procedure);
 	}
 
+	@Cacheable(cacheNames= "undergoes_stay", key= "#stay.stayId")
 	public List<Undergoes> getUndergoesByStayId(Stay stay) {
 		return undergoesPersistence.findByStay(stay);
 	}
 
+	@Cacheable(cacheNames= "undergoes_done", key= "#root.methodName")
 	public List<Undergoes> getUndergoesDone() {
 		return undergoesPersistence.getUndergoesDone();
 	}
 
+	@Cacheable(cacheNames= "undergoes_not_done", key= "#root.methodName")
 	public List<Undergoes> getUndergoesNotDone() {
 		return undergoesPersistence.getUndergoesNotDone();
 	}
 	
+	@Cacheable(cacheNames= "undergoes_block_date", key= "{#blockId + #date}")
+	public List<Undergoes> getNurseUndergoesByBlockIdAndDate(int blockId, Date date) {
+		return undergoesPersistence.getUndergoesByBlockIdAndDate(blockId, date);
+	}
+
+	@Cacheable(cacheNames= "undergoes_block_today", key= "{today: #blockId}")
+	public List<Undergoes> getUndergoesTodayByNurseId(int nurseId) {
+		return undergoesPersistence.getTodayUndergoesByNurseId(nurseId);
+	}
+	
+	@Cacheable(cacheNames= "undergoes", key= "#undergoes.id")
 	public Undergoes setUndergoes(Undergoes undergoes) {
 		return undergoesPersistence.save(undergoes);
 	}
 	
+	@CachePut(cacheNames= "undergoes", key= "#undergoes.id")
 	public Undergoes updateUndergoes(Undergoes undergoes) {
 		Undergoes undergoesTmp = undergoesPersistence.findById(undergoes.getUndergoesId())
 								.orElseThrow(() -> new EntityNotFoundException(String.valueOf(undergoes.getUndergoesId())));

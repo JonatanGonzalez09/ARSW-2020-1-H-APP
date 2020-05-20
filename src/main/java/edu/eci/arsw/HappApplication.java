34f7fc6a1caf80b6e619309a6380d8ecff0067a3
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-05-19 20:45:13 -0500

    correction Web Socket

--- src/main/java/edu/eci/arsw/HappApplication.java
+++ src/main/java/edu/eci/arsw/HappApplication.java
@@ -3,6 +3,9 @@ package edu.eci.arsw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableCaching
@@ -12,4 +15,13 @@ public class HappApplication {
 		SpringApplication.run(HappApplication.class, args);
 	}
 
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+            }
+        };
+    }
 }
